26790fb7dc938591f33978626d295fcc109c4c00
Raquel Joseph <raquelajoseph@gmail.com>
2021-03-05 22:59:20 -0500

    Update model.rb

--- models/model.rb
+++ models/model.rb
@@ -1,16 +1,16 @@
 require 'sqlite3'
 
-# db.execute "CREATE TABLE IF NOT EXISTS user_timesheet(company TEXT, date TEXT, hours TEXT, signature TEXT)"
+db.execute "CREATE TABLE IF NOT EXISTS user_timesheet(company TEXT, date TEXT, hours TEXT, signature TEXT)"
 
-# def add(db,organization,date,hours,signature)
-#     db.execute "INSERT INTO user_timesheet(company, date, hours, signature) VALUES (?,?,?,?)", organization,date,hours,signature
-# end
+def add(db,organization,date,hours,signature)
+    db.execute "INSERT INTO user_timesheet(company, date, hours, signature) VALUES (?,?,?,?)", organization,date,hours,signature
+end
     
-# add(db,"nbc","04/22/18","5","jessica sanchez")
+add(db,"nbc","04/22/18","5","jessica sanchez")
 
-#     rescue SQLite3::Exception => e 
-#         puts "Exception occurred"
-#         puts e
-#     ensure
-#         db.close if db
-# end
+    rescue SQLite3::Exception => e 
+        puts "Exception occurred"
+        puts e
+    ensure
+        db.close if db
+end
