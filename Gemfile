7afeb3f9686aebeaf46429f425ab31734b3f6d87
raqueljoseph <raquelajoseph@gmail.com>
2021-03-06 17:59:53 -0500

    fix error

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ gem 'pry'
 gem 'sinatra'
 gem 'shotgun'
 gem 'dotenv'
+# gem 'rack'
 group :development, :test do
   gem 'sqlite3'
 end
+
+gem "webrick", "~> 1.7"
--- models/model.rb
+++ models/model.rb
@@ -1,12 +1,13 @@
 require 'sqlite3'
 
+db = SQLite3::Database.open 'timesheet.db'
 db.execute "CREATE TABLE IF NOT EXISTS user_timesheet(company TEXT, date TEXT, hours TEXT, signature TEXT)"
 
 def add(db,organization,date,hours,signature)
     db.execute "INSERT INTO user_timesheet(company, date, hours, signature) VALUES (?,?,?,?)", organization,date,hours,signature
 end
     
-add(db,"nbc","04/22/18","5","jessica sanchez")
+# add(db,"nbc","04/22/18","5","jessica sanchez")
 
 #     rescue SQLite3::Exception => e 
 #         puts "Exception occurred"
