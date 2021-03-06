8f962e88c86c36237fcbab19cedf722cfa204596
raqueljoseph <raquelajoseph@gmail.com>
2021-03-05 21:51:05 -0500

    add test.html

--- models/model.rb
+++ models/model.rb
@@ -1,1 +1,19 @@
-$sqlite3 Databasename.rb
+require 'sqlite3'
+
+begin
+    
+    db = SQLite3::Database.open 'timesheet.db'
+    db.execute "CREATE TABLE IF NOT EXISTS user_timesheet(company TEXT, date TEXT, hours INT, signature TEXT)"
+    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+    
+    rescue SQLite3::Exception => e 
+        puts "Exception occurred"
+        puts e
+    ensure
+        db.close if db
+end
