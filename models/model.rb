c0518ca86706b8511fd00feaa77fa62d2645c12c
Raquel Joseph <raquelajoseph@gmail.com>
2021-03-05 22:09:33 -0500

    Update model.rb

--- models/model.rb
+++ models/model.rb
@@ -1,19 +1,19 @@
-require 'sqlite3'
+# require 'sqlite3'
 
-begin
+# begin
     
-    db = SQLite3::Database.open 'timesheet.db'
-    db.execute "CREATE TABLE IF NOT EXISTS user_timesheet(company TEXT, date TEXT, hours INT, signature TEXT)"
-    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
-    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
-    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
-    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
-    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
-    db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+#     db = SQLite3::Database.open 'timesheet.db'
+#     db.execute "CREATE TABLE IF NOT EXISTS user_timesheet(company TEXT, date TEXT, hours INT, signature TEXT)"
+#     db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+#     db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+#     db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+#     db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+#     db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
+#     db.execute 'INSERT INTO user_timesheet(company, date, hours, signature) VALUES ("google", "03/22", 5 ,"Raquel Joseph")'
     
-    rescue SQLite3::Exception => e 
-        puts "Exception occurred"
-        puts e
-    ensure
-        db.close if db
-end
+#     rescue SQLite3::Exception => e 
+#         puts "Exception occurred"
+#         puts e
+#     ensure
+#         db.close if db
+# end
