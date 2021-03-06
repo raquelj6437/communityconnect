5bb5f6eabb13ce3b249548f648f8c92fa8d48342
Raquel Joseph <raquelajoseph@gmail.com>
2021-03-05 22:58:02 -0500

    Update model.rb

--- models/model.rb
+++ models/model.rb
@@ -1,10 +1,10 @@
-require 'sqlite3'
+# require 'sqlite3'
 
-db.execute "CREATE TABLE IF NOT EXISTS user_timesheet(company TEXT, date TEXT, hours TEXT, signature TEXT)"
+# db.execute "CREATE TABLE IF NOT EXISTS user_timesheet(company TEXT, date TEXT, hours TEXT, signature TEXT)"
 
-def add(db,organization,date,hours,signature)
-    db.execute "INSERT INTO user_timesheet(company, date, hours, signature) VALUES (?,?,?,?)", organization,date,hours,signature
-end
+# def add(db,organization,date,hours,signature)
+#     db.execute "INSERT INTO user_timesheet(company, date, hours, signature) VALUES (?,?,?,?)", organization,date,hours,signature
+# end
     
 # add(db,"nbc","04/22/18","5","jessica sanchez")
 
