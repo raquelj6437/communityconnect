60b4f77ae4a459ca37ac49a2f142ef4bd104f9c8
raqueljoseph <raquelajoseph@gmail.com>
2021-03-15 12:33:45 -0400

    add services controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :opportunities
+  resources :opportunities, :services
   root "opportunities#index"
-
-  get '/new' => 'opportunities#new'
 end
