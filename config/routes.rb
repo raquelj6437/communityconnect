e1b33c203db202f0fdf5a0341cdbf37c31d97c17
adriana alvarado <alvadri@bu.edu>
2021-04-06 14:49:44 -0400

    foreman, running react

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :opportunities, :services, :users
