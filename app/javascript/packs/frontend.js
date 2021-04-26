7aba1f189cd8573f04461c2ae35631001dac01d2
adriana alvarado <alvadri@bu.edu>
2021-04-25 21:25:36 -0400

    final login and registration styling + js

--- app/javascript/packs/frontend.js
+++ app/javascript/packs/frontend.js
@@ -1,3 +1,4 @@
+window.onload=function(){
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -7,3 +8,4 @@ signUpButton.addEventListener('click', () =>
 
 signInButton.addEventListener('click', () => 
 	container.classList.remove("right-panel-active"));
+}
